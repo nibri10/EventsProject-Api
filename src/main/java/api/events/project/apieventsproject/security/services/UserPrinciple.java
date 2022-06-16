a15793e03c26e4b37842e6c755c129f7a1a3c1d1
Nicolas Oliveira <nibri.kcond2011@gmail.com>
2022-06-16 16:52:10 -0300

    disabled

--- src/main/java/api/events/project/apieventsproject/security/services/UserPrinciple.java
+++ src/main/java/api/events/project/apieventsproject/security/services/UserPrinciple.java
@@ -29,28 +29,27 @@ public class UserPrinciple implements UserDetails {
     private Collection<? extends GrantedAuthority> authorities;
 
     public UserPrinciple(Long id, String name, 
-			    		String username, String email, String password, 
-			    		Collection<? extends GrantedAuthority> authorities) {
+			    		String username, String email, String password) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
-        this.authorities = authorities;
+//        this.authorities = authorities;
     }
 
     public static UserPrinciple build(User user) {
-        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
-                new SimpleGrantedAuthority(role.getName().name())
-        ).collect(Collectors.toList());
+//        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
+//                new SimpleGrantedAuthority(role.getName().name())
+//        ).collect(Collectors.toList());
 
         return new UserPrinciple(
                 user.getId(),
                 user.getName(),
                 user.getUsername(),
                 user.getEmail(),
-                user.getPassword(),
-                authorities
+                user.getPassword()
+//                authorities
         );
     }
 
